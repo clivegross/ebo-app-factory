874f4f4df49b2114fb7968d9e2f74aecf37affdf
Clive GROSS <SESA368046@se.com>
2025-06-13 15:09:43 +1000

    added sum alarm and alarm view support

--- src/ebo_app_factory/alarm_builder.py
+++ src/ebo_app_factory/alarm_builder.py
@@ -7,42 +7,31 @@ class EBOAlarmBuilder(EBOXMLBuilder):
     A class to build Alarm XML objects for EBO import.
     """
 
+    CHANGE_OF_STATE_ALARM_TYPE = "alarm.ChangeOfStateAlarm"
+    SUM_ALARM_TYPE = "alarm.SumAlarm"
+    EVENT_FILTER_TYPE = "event.filter.Filter"
+    ALARM_VIEWER_TYPE = "alarm.AlarmViewer"
+
     def __init__(self, ebo_version="6.0.4.90", server_full_path="/Server 1"):
         super().__init__(ebo_version, server_full_path)
 
-    def create_change_of_state_alarm(
+    def _create_alarm_oi(
         self,
         name,
+        alarm_type,
         description=None,
         note1=None,
         note2=None,
         alarm_message=None,
         reset_message=None,
         category=None,
-        monitored_variable=None,
         priority=None,
         attachment=None,
-        **kwargs,
+        extra_pis=None,
     ):
-        """
-        Create a ChangeOfStateAlarm XML element.
-
-        :param name: The name of the alarm (required).
-        :param description: Optional description for the alarm.
-        :param note1: Optional NOTE1.
-        :param note2: Optional NOTE2.
-        :param alarm_message: Optional alarm message.
-        :param reset_message: Optional reset message.
-        :param category: Optional category reference path.
-        :param monitored_variable: Optional monitored variable reference path.
-        :param priority: Optional alarm priority (int or str).
-        :param attachment: Optional attachment object reference.
-        :param kwargs: Additional parameters (not used in this method).
-        :return: An XML element representing the ChangeOfStateAlarm.
-        """
         attribs = {
             "NAME": name,
-            "TYPE": "alarm.ChangeOfStateAlarm",
+            "TYPE": alarm_type,
         }
         if description is not None:
             attribs["DESCR"] = description
@@ -71,29 +60,19 @@ class EBOAlarmBuilder(EBOXMLBuilder):
                     "TransferRate": "10",
                 },
             )
-        if monitored_variable is not None:
-            mon_pi = ET.SubElement(alarm, "PI", {"Name": "MonitoredVariable"})
-            ET.SubElement(
-                mon_pi,
-                "Reference",
-                {
-                    "DeltaFilter": "0",
-                    "Object": monitored_variable,
-                    "Property": "Value",
-                    "Retransmit": "0",
-                    "TransferRate": "10",
-                },
-            )
         if note1 is not None:
             ET.SubElement(alarm, "PI", {"Name": "NOTE1", "Value": note1})
         if note2 is not None:
             ET.SubElement(alarm, "PI", {"Name": "NOTE2", "Value": note2})
         if reset_message is not None:
             ET.SubElement(alarm, "PI", {"Name": "ResetMessage", "Value": reset_message})
-        # TODO: add support for TimeDelayToNormal
-        # ET.SubElement(alarm, "PI", {"Name": "TimeDelayToNormal", "Value": "2"})
 
-        # Example: Add an Attachment child (optional, can be parameterized)
+        # Add any extra PI elements (as a list of dicts)
+        if extra_pis:
+            for pi in extra_pis:
+                ET.SubElement(alarm, "PI", pi)
+
+        # Attachment
         if attachment is not None:
             attachment_oi = ET.SubElement(
                 alarm,
@@ -118,3 +97,302 @@ class EBOAlarmBuilder(EBOXMLBuilder):
             )
 
         return alarm
+
+    def _set_monitored_variable(self, obj, monitored_variable):
+        """
+        Example monitored variable XML object:
+        <PI Name="MonitoredVariable">
+            <Reference DeltaFilter="0" Object="../../../../../../../../Interfaces/Modbus TCP Network/Fire Detection System/Ampac FFP Gateway 1/zones_Z1_to_Z1000/Zone Z1 - TOWER 2 BASEMENT 5 - ALARM" Property="Value" Retransmit="0" TransferRate="10"/>
+        </PI>
+        """
+        mon_pi = ET.SubElement(obj, "PI", {"Name": "MonitoredVariable"})
+        ET.SubElement(
+            mon_pi,
+            "Reference",
+            {
+                "DeltaFilter": "0",
+                "Object": monitored_variable,
+                "Property": "Value",
+                "Retransmit": "0",
+                "TransferRate": "10",
+            },
+        )
+        return obj
+
+    def _create_event_filter_condition_oi(self, condition, values, **kwargs):
+        """
+        Create an Event Filter Condition XML object.
+
+        Args:
+            - condition: Name of the filter condition (e.g., "Source", "Category", "AlarmState").
+            - values: List of values for the condition.
+
+        Returns:
+            An XML Element representing the Event Filter Condition.
+
+        Example:
+        self._create_event_filter_condition_oi(condition="Source", values=["* Z1 *- ALARM"])
+
+        returns:
+        <OI NAME="Source" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">
+          <PI Name="Column" Value="Source"/>
+          <PI Name="DisplayName" Value="Source"/>
+          <OI NAME="Value1" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">
+            <PI Name="Value" Value="* Z1 *- ALARM"/>
+          </OI>
+        </OI>
+
+        self._create_event_filter_condition_oi(condition="AlarmState", values=[1, 2])
+
+        returns:
+            <OI NAME="AlarmState" TYPE="event.filter.expression.Enum" flags="aggregated" hidden="1">
+            <PI Name="Column" Value="AlarmState"/>
+            <PI Name="DisplayName" Value="Alarm state"/>
+            <PI Name="EnumType" Value="alarm.pt.AlarmState"/>
+            <OI NAME="1" TYPE="event.filter.expression.EnumValue" flags="aggregated" hidden="1">
+                <PI Name="Value" Value="1"/>
+            </OI>
+            <OI NAME="2" TYPE="event.filter.expression.EnumValue" flags="aggregated" hidden="1">
+                <PI Name="Value" Value="2"/>
+            </OI>
+            </OI>
+        """
+        supported_conditions = {
+            "Source": {
+                "type": "event.filter.expression.Text",
+                "value_type": "event.filter.expression.TextValue",
+            },
+            "Category": {
+                "type": "event.filter.expression.Text",
+                "value_type": "event.filter.expression.TextValue",
+            },
+            "AlarmState": {
+                "type": "event.filter.expression.Enum",
+                "EnumType": "alarm.pt.AlarmState",
+                "value_type": "event.filter.expression.EnumValue",
+            },
+        }
+        if condition not in supported_conditions:
+            raise ValueError(f"Unsupported filter condition: {condition}")
+
+        condition_meta = supported_conditions[condition]
+        oi_attribs = {
+            "NAME": condition,
+            "TYPE": condition_meta["type"],
+            "flags": "aggregated",
+            "hidden": "1",
+        }
+        oi = ET.Element("OI", oi_attribs)
+        ET.SubElement(oi, "PI", {"Name": "Column", "Value": condition})
+        ET.SubElement(
+            oi,
+            "PI",
+            {
+                "Name": "DisplayName",
+                "Value": condition if condition != "AlarmState" else "Alarm state",
+            },
+        )
+        if "EnumType" in condition_meta:
+            ET.SubElement(
+                oi, "PI", {"Name": "EnumType", "Value": condition_meta["EnumType"]}
+            )
+
+        for idx, value in enumerate(values, start=1):
+            if condition_meta["value_type"] == "event.filter.expression.EnumValue":
+                oi_name = f"{idx}"
+            else:
+                oi_name = f"Value{idx}"
+            value_oi_attribs = {
+                "NAME": oi_name,
+                "TYPE": condition_meta["value_type"],
+                "flags": "aggregated",
+                "hidden": "1",
+            }
+            value_oi = ET.SubElement(oi, "OI", value_oi_attribs)
+            ET.SubElement(value_oi, "PI", {"Name": "Value", "Value": str(value)})
+
+        return oi
+
+    def _create_event_filter(self, conditions_values):
+        """
+        Create and return an Event Filter OI element (does NOT attach to parent).
+
+        Args:
+            - conditions_values: Dict mapping condition names to lists of values.
+                Example: {"Source": ["* Z1 *- ALARM"], "Category": ["Fire"], "AlarmState": [1, 2]}
+
+        Returns:
+            The created filter OI element. eg
+            <OI NAME="Filter" TYPE="event.filter.Filter" declared="1" hidden="1">
+                ...conditions
+            </OI>
+        """
+        filter_oi = ET.Element(
+            "OI",
+            {
+                "NAME": "Filter",
+                "TYPE": "event.filter.Filter",
+                "declared": "1",
+                "hidden": "1",
+            },
+        )
+        for condition, values in conditions_values.items():
+            cond_oi = self._create_event_filter_condition_oi(condition, values)
+            filter_oi.append(cond_oi)
+        return filter_oi
+
+    def create_sum_alarm(self, name, conditions_values, **kwargs):
+        """
+        Create a Sum Alarm XML object.
+
+        Args:
+            - name:
+            - filters:
+
+        Example XML structure:
+        <OI NAME="Sum Alarm" TYPE="alarm.SumAlarm">
+            <PI Name="AutoHide" Value="1"/>
+            <PI Name="Category">
+                <Reference DeltaFilter="0" Locked="1" Object="~/System/Alarm Control Panel/Alarm Handling/Categories/Fire" Retransmit="0" TransferRate="10"/>
+            </PI>
+            <OI NAME="Filter" TYPE="event.filter.Filter" declared="1" hidden="1">
+                <OI NAME="Source" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">
+                    <PI Name="Column" Value="Source"/>
+                    <PI Name="DisplayName" Value="Source"/>
+                    <OI NAME="Value1" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">
+                        <PI Name="Value" Value="* Z1 *- ALARM"/>
+                    </OI>
+                </OI>
+                <OI NAME="Category" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">
+                    <PI Name="Column" Value="Category"/>
+                    <PI Name="DisplayName" Value="Category"/>
+                    <OI NAME="Value1" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">
+                        <PI Name="Value" Value="Fire"/>
+                    </OI>
+                </OI>
+                <OI NAME="AlarmState" TYPE="event.filter.expression.Enum" flags="aggregated" hidden="1">
+                    <PI Name="Column" Value="AlarmState"/>
+                    <PI Name="DisplayName" Value="Alarm state"/>
+                    <PI Name="EnumType" Value="alarm.pt.AlarmState"/>
+                    <OI NAME="1" TYPE="event.filter.expression.EnumValue" flags="aggregated" hidden="1">
+                        <PI Name="Value" Value="1"/>
+                    </OI>
+                    <OI NAME="2" TYPE="event.filter.expression.EnumValue" flags="aggregated" hidden="1">
+                        <PI Name="Value" Value="2"/>
+                    </OI>
+                </OI>
+            </OI>
+        </OI>
+        """
+        if not conditions_values:
+            print(
+                "Warning: No filters provided for Sum Alarm. Sum Alarm will capture everything."
+            )
+        alarm = self._create_alarm_oi(
+            name=name, alarm_type=self.SUM_ALARM_TYPE, **kwargs
+        )
+        # Add unique filter structure
+        filter_oi = self._create_event_filter(conditions_values)
+        alarm.append(filter_oi)
+        return alarm
+
+    def create_change_of_state_alarm(
+        self,
+        name,
+        monitored_variable=None,  # unique/required for this alarm type
+        **kwargs,  # all other common/optional params
+    ):
+        """
+        Create a Change of State Alarm XML object.
+
+        Args:
+            - name:
+            - monitored_variable:
+
+        Example XML structure:
+        <OI DESCR="Fire alarm in zone" NAME="Zone Z1 - ALARM" TYPE="alarm.ChangeOfStateAlarm">
+            <PI Name="AlarmMessage" Value="@(SourceObject-&gt;NOTE1) - @(SourceObject-&gt;DESCR)"/>
+            <PI Name="AlarmPriority" Value="1"/>
+            <PI Name="Category">
+            <Reference DeltaFilter="0" Locked="1" Object="~/System/Alarm Control Panel/Alarm Handling/Categories/Fire" Retransmit="0" TransferRate="10"/>
+            </PI>
+            <PI Name="MonitoredVariable">
+            <Reference DeltaFilter="0" Object="../../../../../../../../Interfaces/Modbus TCP Network/Fire Detection System/Ampac FFP Gateway 1/zones_Z1_to_Z1000/Zone Z1 - TOWER 2 BASEMENT 5 - ALARM" Property="Value" Retransmit="0" TransferRate="10"/>
+            </PI>
+            <PI Name="NOTE1" Value="Zone Z1 - ICG-B05"/>
+            <PI Name="NOTE2" Value="IRD-ICG-B05"/>
+            <PI Name="ResetMessage" Value="@(SourceObject-&gt;NOTE1) - @(SourceObject-&gt;DESCR) Returned to Normal"/>
+            <PI Name="ResetPriority" Value="1"/>
+            <OI NAME="Attachment" TYPE="alarm.attachments.Attachment" flags="aggregated" hidden="1">
+            <PI Name="ObjectReference">
+                <Reference DeltaFilter="0" Object="../../../../../_Common/Graphics/ICG-B05 Fire" Retransmit="0" TransferRate="10"/>
+            </PI>
+            </OI>
+            <OI NAME="Attachment_2" TYPE="alarm.attachments.Attachment" flags="aggregated" hidden="1">
+            <PI Name="ObjectReference">
+                <Reference DeltaFilter="0" Object="../../../../../../../_Common/Graphics/ICG-B05 Fire" Retransmit="0" TransferRate="10"/>
+            </PI>
+            </OI>
+        </OI>
+        """
+        alarm = self._create_alarm_oi(
+            name=name,
+            alarm_type=self.CHANGE_OF_STATE_ALARM_TYPE,
+            **kwargs,
+        )
+        if monitored_variable:
+            self._set_monitored_variable(alarm, monitored_variable)
+        return alarm
+
+    def create_alarm_view(
+        self,
+        name="Alarm View",
+        conditions_values=None,
+        description=None,
+        note1=None,
+        note2=None,
+    ):
+        """
+        Create Alarm View XML object.
+
+        Example:
+        <OI NAME="Alarm View" TYPE="alarm.AlarmViewer">
+        <OI NAME="Filter" TYPE="event.filter.Filter" declared="1" hidden="1">
+            <OI NAME="Category" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">
+            <PI Name="Column" Value="Category"/>
+            <PI Name="DisplayName" Value="Category"/>
+            <PI Name="Operator" Value="0"/>
+            <OI NAME="Value1" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">
+                <PI Name="Value" Value="Fire"/>
+            </OI>
+            </OI>
+            <OI NAME="Source" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">
+            <PI Name="Column" Value="Source"/>
+            <PI Name="DisplayName" Value="Source"/>
+            <PI Name="Operator" Value="0"/>
+            <OI NAME="Value1" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">
+                <PI Name="Value" Value="* Z1 *- ALARM"/>
+            </OI>
+            <OI NAME="Value2" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">
+                <PI Name="Value" Value="*Sum Alarm*"/>
+            </OI>
+            </OI>
+        </OI>
+        </OI>
+        """
+        attribs = {
+            "NAME": name,
+            "TYPE": self.ALARM_VIEWER_TYPE,
+        }
+        if description is not None:
+            attribs["DESCR"] = description
+
+        view = ET.Element("OI", attribs)
+        if note1 is not None:
+            ET.SubElement(view, "PI", {"Name": "NOTE1", "Value": note1})
+        if note2 is not None:
+            ET.SubElement(view, "PI", {"Name": "NOTE2", "Value": note2})
+        # Add unique filter structure
+        filter_oi = self._create_event_filter(conditions_values)
+        view.append(filter_oi)
+        return view
--- tests/test_alarm_builder.py
+++ tests/test_alarm_builder.py
@@ -83,3 +83,233 @@ def test_create_change_of_state_alarm(tmp_path):
         '<Reference DeltaFilter="0" Object="../../../../../_Common/Graphics/ICG-B05 Fire" Retransmit="0" TransferRate="10"/>'
         in xml_from_file
     )
+
+
+# Recreate this Sum Alarm object
+# <OI NAME="Sum Alarm" TYPE="alarm.SumAlarm">
+#   <PI Name="AutoHide" Value="1"/>
+#   <PI Name="Category">
+#     <Reference DeltaFilter="0" Locked="1" Object="~/System/Alarm Control Panel/Alarm Handling/Categories/Fire" Retransmit="0" TransferRate="10"/>
+#   </PI>
+#         <PI Name="AlarmMessage" Value="@(SourceObject-&gt;NOTE1) - @(SourceObject-&gt;DESCR)"/>
+#         <PI Name="ResetMessage" Value="@(SourceObject-&gt;NOTE1) - @(SourceObject-&gt;DESCR) Returned to Normal"/>
+#         <PI Name="AlarmPriority" Value="1"/>
+#         <PI Name="ResetPriority" Value="1"/>
+#         <PI Name="NOTE1" Value="Fire Detection Zone Z1"/>
+#         <PI Name="NOTE2" Value="IRD-ICG-B05"/>
+#   <OI NAME="Filter" TYPE="event.filter.Filter" declared="1" hidden="1">
+#     <OI NAME="Source" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">
+#       <PI Name="Column" Value="Source"/>
+#       <PI Name="DisplayName" Value="Source"/>
+#       <OI NAME="Value1" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">
+#         <PI Name="Value" Value="* Z1 *- ALARM"/>
+#       </OI>
+#     </OI>
+#     <OI NAME="Category" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">
+#       <PI Name="Column" Value="Category"/>
+#       <PI Name="DisplayName" Value="Category"/>
+#       <OI NAME="Value1" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">
+#         <PI Name="Value" Value="Fire"/>
+#       </OI>
+#     </OI>
+#     <OI NAME="AlarmState" TYPE="event.filter.expression.Enum" flags="aggregated" hidden="1">
+#       <PI Name="Column" Value="AlarmState"/>
+#       <PI Name="DisplayName" Value="Alarm state"/>
+#       <PI Name="EnumType" Value="alarm.pt.AlarmState"/>
+#       <OI NAME="1" TYPE="event.filter.expression.EnumValue" flags="aggregated" hidden="1">
+#         <PI Name="Value" Value="1"/>
+#       </OI>
+#       <OI NAME="2" TYPE="event.filter.expression.EnumValue" flags="aggregated" hidden="1">
+#         <PI Name="Value" Value="2"/>
+#       </OI>
+#     </OI>
+#   </OI>
+# </OI>
+def test_create_sum_alarm(tmp_path):
+    builder = EBOAlarmBuilder(ebo_version="5.0.3.117")
+    alarm = builder.create_sum_alarm(
+        name="Sum Alarm",
+        alarm_message="@(SourceObject->NOTE1) - @(SourceObject->DESCR)",
+        reset_message="@(SourceObject->NOTE1) - @(SourceObject->DESCR) Returned to Normal",
+        priority=1,
+        category="~/System/Alarm Control Panel/Alarm Handling/Categories/Fire",
+        note1="Fire Detection Zone Z1",
+        note2="IRD-ICG-B05",
+        attachment="../../../../../_Common/Graphics/ICG-B05 Fire",
+        conditions_values={
+            "Source": ["* Z1 *- ALARM"],
+            "Category": ["Fire"],
+            "AlarmState": [1, 2],
+        },
+    )
+    builder.add_to_exported_objects(alarm)
+    xml_str = builder.to_pretty_xml()
+    print(xml_str)
+    # Write to file
+    output_path = os.path.join(tmp_path, "tmp_output.xml")
+    builder.write_xml(output_path)
+    assert os.path.exists(output_path), f"Output file {output_path} does not exist"
+    with open(output_path, "r", encoding="utf-8") as f:
+        xml_from_file = f.read()
+    # Basic checks
+    assert '<SourceVersion Value="5.0.3.117"/>' in xml_from_file
+    assert '<OI NAME="Sum Alarm" TYPE="alarm.SumAlarm">' in xml_from_file
+    assert (
+        '<PI Name="AlarmMessage" Value="@(SourceObject-&gt;NOTE1) - @(SourceObject-&gt;DESCR)"/>'
+        in xml_from_file
+    )
+    assert '<PI Name="AlarmPriority" Value="1"/>' in xml_from_file
+    assert '<PI Name="Category">' in xml_from_file
+    assert (
+        '<Reference DeltaFilter="0" Locked="1" Object="~/System/Alarm Control Panel/Alarm Handling/Categories/Fire" Retransmit="0" TransferRate="10"/>'
+        in xml_from_file
+    )
+    assert '<PI Name="NOTE1" Value="Fire Detection Zone Z1"/>' in xml_from_file
+    assert '<PI Name="NOTE2" Value="IRD-ICG-B05"/>' in xml_from_file
+    assert (
+        '<PI Name="ResetMessage" Value="@(SourceObject-&gt;NOTE1) - @(SourceObject-&gt;DESCR) Returned to Normal"/>'
+        in xml_from_file
+    )
+    assert '<PI Name="ResetPriority" Value="1"/>' in xml_from_file
+    assert (
+        '<OI NAME="Attachment" TYPE="alarm.attachments.Attachment" flags="aggregated" hidden="1">'
+        in xml_from_file
+    )
+    assert '<PI Name="ObjectReference">' in xml_from_file
+    assert (
+        '<Reference DeltaFilter="0" Object="../../../../../_Common/Graphics/ICG-B05 Fire" Retransmit="0" TransferRate="10"/>'
+        in xml_from_file
+    )
+    # check filter
+    assert (
+        '<OI NAME="Filter" TYPE="event.filter.Filter" declared="1" hidden="1">'
+        in xml_from_file
+    )
+    # Source filter
+    assert (
+        '<OI NAME="Source" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">'
+        in xml_from_file
+    )
+    assert '<PI Name="Column" Value="Source"/>' in xml_from_file
+    assert '<PI Name="DisplayName" Value="Source"/>' in xml_from_file
+    assert (
+        '<OI NAME="Value1" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">'
+        in xml_from_file
+    )
+    assert '<PI Name="Value" Value="* Z1 *- ALARM"/>' in xml_from_file
+    # Category filter
+    assert (
+        '<OI NAME="Category" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">'
+        in xml_from_file
+    )
+    assert '<PI Name="Column" Value="Category"/>' in xml_from_file
+    assert '<PI Name="DisplayName" Value="Category"/>' in xml_from_file
+    assert (
+        '<OI NAME="Value1" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">'
+        in xml_from_file
+    )
+    assert '<PI Name="Value" Value="Fire"/>' in xml_from_file
+    # AlarmState filter
+    assert (
+        '<OI NAME="AlarmState" TYPE="event.filter.expression.Enum" flags="aggregated" hidden="1">'
+        in xml_from_file
+    )
+    assert '<PI Name="Column" Value="AlarmState"/>' in xml_from_file
+    assert '<PI Name="DisplayName" Value="Alarm state"/>' in xml_from_file
+    assert '<PI Name="EnumType" Value="alarm.pt.AlarmState"/>' in xml_from_file
+    assert (
+        '<OI NAME="1" TYPE="event.filter.expression.EnumValue" flags="aggregated" hidden="1">'
+        in xml_from_file
+    )
+    assert '<PI Name="Value" Value="1"/>' in xml_from_file
+    assert (
+        '<OI NAME="2" TYPE="event.filter.expression.EnumValue" flags="aggregated" hidden="1">'
+        in xml_from_file
+    )
+    assert '<PI Name="Value" Value="2"/>' in xml_from_file
+
+
+# Recreate this Alarm View object:
+# <OI NAME="Alarm View" TYPE="alarm.AlarmViewer">
+#   <OI NAME="Filter" TYPE="event.filter.Filter" declared="1" hidden="1">
+#     <OI NAME="Category" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">
+#       <PI Name="Column" Value="Category"/>
+#       <PI Name="DisplayName" Value="Category"/>
+#       <PI Name="Operator" Value="0"/>
+#       <OI NAME="Value1" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">
+#         <PI Name="Value" Value="Fire"/>
+#       </OI>
+#     </OI>
+#     <OI NAME="Source" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">
+#       <PI Name="Column" Value="Source"/>
+#       <PI Name="DisplayName" Value="Source"/>
+#       <PI Name="Operator" Value="0"/>
+#       <OI NAME="Value1" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">
+#         <PI Name="Value" Value="* Z1 *- ALARM"/>
+#       </OI>
+#       <OI NAME="Value2" TYPE="event.filter.expression.TextValue" flags="aggregated" hidden="1">
+#         <PI Name="Value" Value="*Sum Alarm*"/>
+#       </OI>
+#     </OI>
+#   </OI>
+# </OI>
+def test_create_alarm_view(tmp_path):
+    builder = EBOAlarmBuilder(ebo_version="5.0.3.117")
+
+    alarm_view = builder.create_alarm_view(
+        name="My Alarm View",
+        description="Test Alarm Viewer",
+        note1="This is a test NOTE1",
+        note2="This is a test NOTE2",
+        conditions_values={
+            "Category": ["Fire"],
+            "Source": ["* Z1 *- ALARM", "*Sum Alarm*"],
+        },
+    )
+
+    builder.add_to_exported_objects(alarm_view)
+    xml_str = builder.to_pretty_xml()
+    print(xml_str)
+
+    output_path = os.path.join(tmp_path, "alarm_view.xml")
+    builder.write_xml(output_path)
+
+    assert os.path.exists(output_path), "Output XML file not created"
+
+    with open(output_path, "r", encoding="utf-8") as f:
+        xml = f.read()
+
+    # Header and object
+    assert '<SourceVersion Value="5.0.3.117"/>' in xml
+    assert '<OI NAME="My Alarm View" TYPE="alarm.AlarmViewer"' in xml
+    assert 'DESCR="Test Alarm Viewer"' in xml
+
+    # NOTE1 and NOTE2
+    assert '<PI Name="NOTE1" Value="This is a test NOTE1"/>' in xml
+    assert '<PI Name="NOTE2" Value="This is a test NOTE2"/>' in xml
+
+    # Filter root
+    assert (
+        '<OI NAME="Filter" TYPE="event.filter.Filter" declared="1" hidden="1">' in xml
+    )
+
+    # Category filter block
+    assert (
+        '<OI NAME="Category" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">'
+        in xml
+    )
+    assert '<PI Name="Column" Value="Category"/>' in xml
+    assert '<PI Name="DisplayName" Value="Category"/>' in xml
+    assert '<PI Name="Value" Value="Fire"/>' in xml
+
+    # Source filter block
+    assert (
+        '<OI NAME="Source" TYPE="event.filter.expression.Text" flags="aggregated" hidden="1">'
+        in xml
+    )
+    assert '<PI Name="Column" Value="Source"/>' in xml
+    assert '<PI Name="DisplayName" Value="Source"/>' in xml
+    # TODO: is this operator prop required?
+    # assert '<PI Name="Operator" Value="0"/>' in xml
+    assert '<PI Name="Value" Value="* Z1 *- ALARM"/>' in xml
+    assert '<PI Name="Value" Value="*Sum Alarm*"/>' in xml
